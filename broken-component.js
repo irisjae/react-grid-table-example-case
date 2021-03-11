01c7bc12897a8e4d3fc1804f77b32d1544e7c543
irisjae <14106824+irisjae@users.noreply.github.com>
2021-03-11 12:23:44 +0800

    Update broken-component.js

--- broken-component.js
+++ broken-component.js
@@ -1,6 +1,5 @@
 let React = require ('react')
 let ReactDOM = require ('react-dom')
-let { useState, useEffect } = require ('react')
 let GridTable = require ('@nadavshaar/react-grid-table') .default
 document .addEventListener ('DOMContentLoaded', _ => {
 	ReactDOM .render (React .createElement (GridTable, null), document .querySelector ('div'));
